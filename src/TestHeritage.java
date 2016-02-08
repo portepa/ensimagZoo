3ffbc884fcd356115c463e50039a2e4437299a2e
BNilsou <nils.burlat@gmail.com>
2016-02-08 13:48:43 +0100

    Envoi au prof

--- src/TestHeritage.java
+++ src/TestHeritage.java
@@ -3,26 +3,26 @@ public class TestHeritage {
 		// ENLEVER L'ABSTRACT DE ANIMAL POUR EXECUTER CA
 
 		
-		// Minotor alphonse = new Minotor("Alphonse", 453, 4);
-		// Animal minotor = alphonse;
+		Minotor alphonse = new Minotor("Alphonse", 453, 4);
+		Animal minotor = alphonse;
 
-		// System.out.println("Faisons crier Alphonse deux fois :");
-		// alphonse.crier();
-		// minotor.crier();
+		System.out.println("Faisons crier Alphonse deux fois :");
+		alphonse.crier();
+		minotor.crier();
 
-		// System.out.println("Oh, les "
-		// 	+ alphonse.getNombreDeCornes() + " jolies cornes !");
-		// System.out.println("Oh, les "
-		// 	+ minotor.getNombreDeCornes() + " jolies cornes !");
+		System.out.println("Oh, les "
+			+ alphonse.getNombreDeCornes() + " jolies cornes !");
+		System.out.println("Oh, les "
+			+ minotor.getNombreDeCornes() + " jolies cornes !");
 		
-		// /**************************/
-		// Centaure edouard = new Centaure("Edouard", 126, 12);
-		// Animal unAnimal = edouard;
+		/**************************/
+		Centaure edouard = new Centaure("Edouard", 126, 12);
+		Animal unAnimal = edouard;
 
-		// unAnimal = new Centaure("Dominique", 126, 12);
-		// Centaure dominique = (Centaure) unAnimal;
+		unAnimal = new Centaure("Dominique", 126, 12);
+		Centaure dominique = (Centaure) unAnimal;
 
-		// unAnimal = new Animal("Dominique", 126);
-		// dominique = (Centaure) unAnimal;
+		unAnimal = new Animal("Dominique", 126);
+		dominique = (Centaure) unAnimal;
 	}
 }
